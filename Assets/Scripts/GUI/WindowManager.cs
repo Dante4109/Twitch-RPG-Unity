e39e21bb287eef4ef90c7437b806b6af0b5d73d1
RJ Zeller <rogerjohnmorellizeller@gmail.com>
2019-01-17 00:10:01 -0500

    Set chat and console window to both be scrollable

---
+++ Assets/Scripts/GUI/WindowManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowManager : MonoBehaviour
+{
+
+    public static WindowManager Instance { get; private set; }
+
+    [Header("UI Components")]
+    public Canvas MainGuiCanvas;
+    public GameObject MainPanelViewPoint;
+    public GameObject Console_Window;
+    public GameObject Chat_Window;
+
+
+
+
+    public void Display_Console()
+    {
+        Chat_Window.gameObject.SetActive(false);
+        Console_Window.gameObject.SetActive(true);
+
+    }
+
+    public void Display_Chat()
+    {
+        Console_Window.gameObject.SetActive(false);
+        Chat_Window.gameObject.SetActive(true);
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Game Manager/GameManager.cs
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -1,18 +1,47 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
-    {
+    public static GameManager Instance { get; private set; }
 
+    [Header("UI Components")]
+    public Canvas MainGuiCanvas;
+    public ScrollRect scrollrectobject;
+    public Button ConsoleButton;
+    public Button ChatButton;
+    public GameObject ConsoleWindow;
+    public GameObject ChatWindow;
+    private RectTransform[] RectTransforms = new RectTransform[2];
+
+    public void Awake()
+
+    {
+        ConsoleWindow.SetActive(false);
+        ChatWindow.SetActive(false);
+        RectTransforms[0] = ConsoleWindow.GetComponent<RectTransform>();
+        RectTransforms[1] = ChatWindow.GetComponent<RectTransform>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    
+
+
+    public void Display_Console()
+    {
+        ChatWindow.gameObject.SetActive(false);
+        ConsoleWindow.gameObject.SetActive(true);
+        scrollrectobject.content = RectTransforms[0];
+    }
+
+    public void Display_Chat()
+    {
+        ConsoleWindow.gameObject.SetActive(false);
+        ChatWindow.gameObject.SetActive(true);
+        scrollrectobject.content = RectTransforms[1];
+    }
 }
